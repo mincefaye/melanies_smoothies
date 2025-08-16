b2b146c528dabf92465230edfd1bdd783baa8f49
Fince Maye T. Delos Reyes <102656446+mincefaye@users.noreply.github.com>
2025-08-17 03:38:03 +0800

    Update streamlit_app.py
    To display smoothiefruit nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
     st.stop()
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
